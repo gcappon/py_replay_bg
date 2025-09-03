ea0c9608675afedd4b03b1e8af1b492f28d57d26
Giacomo Cappon <cappongiacomo@gmail.com>
2025-09-03 17:38:11 +0200

    updated .toml

--- py_replay_bg/model/model_step_equations_t1d.py
+++ py_replay_bg/model/model_step_equations_t1d.py
@@ -46,30 +46,104 @@ def twin_single_meal_exercise(tsteps, x, A, B,
     return x
 
 
-@njit
-def twin_multi_meal(tsteps, x, A, B,
-                        bolus_delayed, basal_delayed,
-                        meal_B_delayed, meal_L_delayed, meal_D_delayed, meal_S_delayed, meal_H, t_hour,
-                        r1, r2, kgri, kd, p2, SI_B, SI_L, SI_D, VI, VG, Ipb, SG, Gb,
-                        f, kabs_B, kabs_L, kabs_D, kabs_S, kabs_H, alpha, previous_Ra):
-    """
-    Internal function that simulates the multi-meal model using backward-euler method.
-    """
-    # TODO: further optimize knowing that the input never changes. Consider using something already existing.
-    # Run simulation
-    for k in np.arange(1, tsteps):
-        # Integration step
-        x[:, k] = model_step_equations_multi_meal(A, bolus_delayed[k - 1] + basal_delayed[k - 1],
-                                                       meal_B_delayed[k - 1], meal_L_delayed[k - 1],
-                                                       meal_D_delayed[k - 1], meal_S_delayed[k - 1], meal_H[k - 1],
-                                                       t_hour[k - 1], x[:, k - 1], B,
-                                                       r1, r2, kgri, kd, p2, SI_B, SI_L,
-                                                       SI_D, VI,
-                                                       VG, Ipb, SG, Gb, f, kabs_B, kabs_L,
-                                                       kabs_D,
-                                                       kabs_S, kabs_H, alpha, previous_Ra[k-1])
-    return x
+# @njit
+# def twin_multi_meal(tsteps, x, A, B,
+#                         bolus_delayed, basal_delayed,
+#                         meal_B_delayed, meal_L_delayed, meal_D_delayed, meal_S_delayed, meal_H, t_hour,
+#                         r1, r2, kgri, kd, p2, SI_B, SI_L, SI_D, VI, VG, Ipb, SG, Gb,
+#                         f, kabs_B, kabs_L, kabs_D, kabs_S, kabs_H, alpha, previous_Ra):
+#     """
+#     Internal function that simulates the multi-meal model using backward-euler method.
+#     """
+#     # TODO: further optimize knowing that the input never changes. Consider using something already existing.
+#     # Run simulation
+#     for k in np.arange(1, tsteps):
+#         # Integration step
+#         x[:, k] = model_step_equations_multi_meal(A, bolus_delayed[k - 1] + basal_delayed[k - 1],
+#                                                        meal_B_delayed[k - 1], meal_L_delayed[k - 1],
+#                                                        meal_D_delayed[k - 1], meal_S_delayed[k - 1], meal_H[k - 1],
+#                                                        t_hour[k - 1], x[:, k - 1], B,
+#                                                        r1, r2, kgri, kd, p2, SI_B, SI_L,
+#                                                        SI_D, VI,
+#                                                        VG, Ipb, SG, Gb, f, kabs_B, kabs_L,
+#                                                        kabs_D,
+#                                                        kabs_S, kabs_H, alpha, previous_Ra[k-1])
+#     return x
 
+@njit(fastmath=True, cache=True)
+def twin_multi_meal(tsteps, x, A, B,
+                    bolus_delayed, basal_delayed,
+                    meal_B_delayed, meal_L_delayed, meal_D_delayed, meal_S_delayed, meal_H, t_hour,
+                    r1, r2, kgri, kd, p2, SI_B, SI_L, SI_D, VI, VG, Ipb, SG, Gb,
+                    f, kabs_B, kabs_L, kabs_D, kabs_S, kabs_H, alpha, previous_Ra):
+
+    logGb = np.log(Gb)
+    log60 = np.log(60.0)
+    kc = 1.0 / (1.0 + kgri)
+    kd_fac = 1.0 / (1.0 + kd)
+
+    A = np.ascontiguousarray(A)
+    B = np.ascontiguousarray(B)
+    tmp = np.empty(18, dtype=x.dtype)
+
+    # assume B is shape (18,) passed in; will mutate in-place each step
+    # indices for readability
+    # B structure: [B0, B1, B2,  L0,L1,L2,  D0,D1,D2,  S0,S1,S2,  H0,H1,H2,  I0,I1,I2]
+    for k in range(1, tsteps):
+        I = bolus_delayed[k-1] + basal_delayed[k-1]
+
+        # SI by day-part
+        hour = t_hour[k-1]
+        if (hour < 4.0) or (hour >= 17.0):
+            SI = SI_D
+        elif hour < 11.0:
+            SI = SI_B
+        else:
+            SI = SI_L
+
+        # risk
+        risk = 1
+        if Gb > x[0, k-1] >= 60:
+            risk = risk + 10 * r1 * (np.log(x[0, k-1]) ** r2 - logGb ** r2) ** 2
+        elif x[0, k-1] < 60:
+            risk = risk + 10 * r1 * (log60 ** r2 - logGb ** r2) ** 2
+
+        # Build B in place (no Python lists, no allocations)
+        B[0]  = meal_B_delayed[k-1] * kc
+        B[1]  = 0.0; B[2]  = 0.0
+        B[3]  = meal_L_delayed[k-1] * kc
+        B[4]  = 0.0; B[5]  = 0.0
+        B[6]  = meal_D_delayed[k-1] * kc
+        B[7]  = 0.0; B[8]  = 0.0
+        B[9]  = meal_S_delayed[k-1] * kc
+        B[10] = 0.0; B[11] = 0.0
+        B[12] = meal_H[k-1] * kc
+        B[13] = 0.0; B[14] = 0.0
+        B[15] = I * kd_fac
+        B[16] = 0.0; B[17] = 0.0
+
+        # shorthand previous state
+        xkm1 = x[:, k-1]
+        # C is just a view; np.dot can consume non-contiguous in Numba/BLAS without us allocating
+        C = xkm1[2:20]
+
+        # state update
+        # xk[2:20] = A @ C + B
+        tmp[:] = x[2:20, k - 1]  # makes a contiguous copy without realloc
+        x[2:20, k] = A.dot(tmp) + B
+
+        x[1, k] = (xkm1[1] + p2 * (SI / VI) * (x[19, k] - Ipb)) / (1.0 + p2)
+
+        Ra = (f * (kabs_B * x[4, k] + kabs_L * x[7, k] + kabs_D * x[10, k] + kabs_S * x[13, k] + kabs_H * x[16, k])
+              + previous_Ra[k-1]) / VG
+
+        denom = 1.0 + SG + (1.0 + r1 * risk) * x[1, k]
+        x[0, k] = (xkm1[0] + SG * Gb + Ra) / denom
+
+        x[20, k] = (xkm1[20] + alpha * x[0, k]) / (1.0 + alpha)
+
+
+    return x
 
 @njit
 def twin_multi_meal_extended(tsteps, x, A, B,
